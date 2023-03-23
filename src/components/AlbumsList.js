992d26fdf568022d62ab03bb908b298a5e1b45d0
kevin5_lee <kevin5_lee@pegatroncorp.com>
2023-03-23 14:52:42 +0800

    add Album list component and pass user prop

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AlbumsList = ({ user }) => {
+  return <div>Albums for {user.name}</div>;
+};
+
+export default AlbumsList;
--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -3,6 +3,7 @@ import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
 import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 const UserListItem = ({ user }) => {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -21,7 +22,11 @@ const UserListItem = ({ user }) => {
     </>
   );
 
-  return <ExpandablePanel header={header}>Content</ExpandablePanel>;
+  return (
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
+  );
 };
 
 export default UserListItem;
